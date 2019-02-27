3032cc049dfcbcf7105f7a988502a620263ff003
HP <HP@192.168.100.3>
2019-02-27 22:36:00 +0530

    Changed Navbar

--- admin/admin.php
+++ admin/admin.php
@@ -28,23 +28,25 @@
         }
 
     ?>
-    <div class="header">
-        <div class="row">
-            <div class="col-lg-10">
-                <h3>NITC Exam Portal</h3>
-            </div>
-            <div class="col-lg-2">
-                <div class="dropdown">
-                    <button class="btn dropdown-toggle" data-toggle="dropdown">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-header text-white">
+            <h3>NITC Exam Portal</h3>
+        </a>
+        <div class="collapse navbar-collapse">
+            <ul class="navbar-nav" style="margin-left:85%">
+                <li class="nav-item dropdown">
+                    
+                    <a class="nav-link dropdown-toggle" data-toggle="dropdown">
                         <?php echo $admin; ?> 
-                    </button>
+                    </a>
                     <div class="dropdown-menu">
                         <a class="dropdown-item" href="../logout.php"> Logout</a>
                     </div>
-                </div>
-            </div>
+                    
+                </li>
+            </ul>
         </div>
-    </div>
+    </nav>
 
     <div class="sidenav">
         <a href="users.php">Users</a>
